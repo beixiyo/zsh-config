ad858537c15fb1373203693a95f4aee24532750f
CJL <2662442385@qq.com>
2026-02-11 01:20:15 +0800

    fix: prevent TERM_PROGRAM interference in yazi function for WSL + WezTerm compatibility

--- functions/yazi.zsh
+++ functions/yazi.zsh
@@ -2,7 +2,8 @@
 # Q 退出
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	command yazi "$@" --cwd-file="$tmp"
+  # 避免 yazi 使用 TERM_PROGRAM 判断终端导致 WSL + WezTerm 异常按键
+	TERM_PROGRAM="" command yazi "$@" --cwd-file="$tmp"
 	IFS= read -r -d '' cwd < "$tmp"
 	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
 	rm -f -- "$tmp"
