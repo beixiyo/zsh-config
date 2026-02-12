e6350e8c2627276680ae7239d1667095c159e1df
ES <2662442385@qq.com>
2026-02-12 16:29:21 +0800

    feat: add keybinding to clear screen in keybindings.zsh for improved user experience

--- keybindings.zsh
+++ keybindings.zsh
@@ -1,3 +1,6 @@
 # 依赖 plugins.zsh 中的 zsh-history-substring-search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+
+# clear screen
+bindkey '^L' clear-screen
